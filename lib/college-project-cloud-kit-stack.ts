863d519b6990adf52e2e859cbf1951158cdd9e69
Subesh Bhandari <subeshb1@gmail.com>
2020-08-24 23:02:06 +0545

    Add first example

--- lib/college-project-cloud-kit-stack.ts
+++ lib/college-project-cloud-kit-stack.ts
@@ -1,9 +1,21 @@
 import * as cdk from '@aws-cdk/core';
+import * as codebuild from '@aws-cdk/aws-codebuild';
 
 export class CollegeProjectCloudKitStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    new codebuild.Project(this, 'MyProject', {
+      buildSpec: codebuild.BuildSpec.fromObject({
+        version: '0.2',
+        phases: {
+          build: {
+            commands: [
+              'echo "Hello, CodeBuild!"',
+            ],
+          },
+        },
+      }),
+    });
   }
 }
