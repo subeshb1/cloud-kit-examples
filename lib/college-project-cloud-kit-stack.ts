10ce0e042a2f5b638ef933a527bf89c2ca5c592e
Subesh Bhandari <subeshb1@gmail.com>
2020-08-24 23:57:19 +0545

    Add code build for project

--- lib/college-project-cloud-kit-stack.ts
+++ lib/college-project-cloud-kit-stack.ts
@@ -5,7 +5,17 @@ export class CollegeProjectCloudKitStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const gitHubSource = codebuild.Source.gitHub({
+      owner: 'subeshb1',
+      repo: 'project-back-end',
+      webhook: true, // optional, default: true if `webhookFilters` were provided, false otherwise
+      webhookFilters: [
+        codebuild.FilterGroup.inEventOf(codebuild.EventAction.PUSH).andBranchIsNot('master'),
+      ], // optional, by default all pushes and Pull Requests will trigger a build
+    });
+
     new codebuild.Project(this, 'MyProject', {
+      source: gitHubSource,
       buildSpec: codebuild.BuildSpec.fromObject({
         version: '0.2',
         phases: {
